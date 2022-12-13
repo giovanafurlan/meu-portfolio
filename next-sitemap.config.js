8b0f2b3e4e95f4e10a8a5c84672c49643d0bd6e3
giovanafurlan <giovananelofurlan@gmail.com>
2022-12-13 16:47:31 -0300

    Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://portfolio-giovana-furlan.netlify.app',
+    siteUrl: process.env.SITE_URL || 'https://portfolio-giovana-furlan.vercel.app',
     generateRobotsTxt: true,
 }
